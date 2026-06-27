665999f4eca03a4c8de848a322de6423b4e1593b
leirb4G <gabriel.bandeira19991@gmail.com>
2026-06-27 10:15:33 -0300

    Validação de CEP, atividade 9

--- Atividade9/buscaCep.js
+++ Atividade9/buscaCep.js
@@ -1,9 +1,18 @@
 function buscarCep() {
     let cepDigitado = document.getElementById("cep").value;
+    cepDigitado = cepDigitado.replace(/-/g, "");
 
     if (cepDigitado === "") {
+        alert("Digite um CEP");
         return;
     }
+
+
+    if (cepDigitado.length !== 8) {
+        alert("CEP tem tamano inválido, digite um CEP com 8 dígitos");
+        return;
+    }
+
     fetch(`https://viacep.com.br/ws/${cepDigitado}/json/`)
     .then(function (resposta) {
         return resposta.json();
